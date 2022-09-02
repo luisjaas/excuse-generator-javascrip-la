66ecc3e676819c59a63ea1c0ee10c2a45b7c73eb
luisjaas <luisjaas98@gmail.com>
2022-09-02 20:17:28 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let quien = ["perro", "hermano", "abuela", "gato"];
+  let accion = ["se comió", "daño", "se llevo", "incendio"];
+  let que = ["mi tarea", "tu celular", "tu carro", "tu laptop"];
+
+  let excuseP = document.querySelector("#excuse");
+  excuseP.textContent =
+    "Mi" +
+    " " +
+    quien[Math.floor(Math.random() * 4)] +
+    " " +
+    accion[Math.floor(Math.random() * 4)] +
+    " " +
+    que[Math.floor(Math.random() * 4)];
+
   console.log("Hello Rigo from the console!");
 };
